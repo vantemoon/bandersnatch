e0768ca6fc0da8beac7bdd4d5489c91edf912605
vantemoon <sylviassunrx0820@outlook.com>
2022-06-18 17:50:55 +0800

    Created in-game setting menu (unfinished)

---
+++ Assets/Scripts/SpriteButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteButton : MonoBehaviour
+{
+    public void OnMouseDown() {
+        Debug.Log("BUTTON PRESSED");
+    }
+}
